cc15861d26ca4136ffe7e52f8604cff12317df9f
Simon <Simon_Freelance@hotmail.com>
2018-07-02 19:14:38 -0400

    Changed class to className attributes

--- incipit/src/components/Modals/AddDeckModal.js
+++ incipit/src/components/Modals/AddDeckModal.js
@@ -4,34 +4,34 @@ import "./../bulma.css";
 
 const AddDeckModal = () => {
   return (
-    <div class="modal">
-      <div class="modal-background" />
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Add Deck</p>
-          <button class="delete" aria-label="close" />
+    <div className="modal">
+      <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Add Deck</p>
+          <button className="delete" aria-label="close" />
         </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <label class="label">Deck Name</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="Name" />
+        <section className="modal-card-body">
+          <div className="field">
+            <label className="label">Deck Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Name" />
             </div>
           </div>
           <div className = "control">
-          <label class="radio">
+          <label className="radio">
             <input type="radio" name="answer" />
             Private?
           </label>
-          <label class="radio">
+          <label className="radio">
             <input type="radio" name="answer" />
             Public?
           </label>
           </div>
         </section>
-        <footer class="modal-card-foot">
-          <button class="button is-info">Add</button>
-          <button class="button">Cancel</button>
+        <footer className="modal-card-foot">
+          <button className="button is-info">Add</button>
+          <button className="button">Cancel</button>
         </footer>
       </div>
     </div>
