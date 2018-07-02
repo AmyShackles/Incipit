82551644514628d9e5495d4f50715488f1a00726
Simon <Simon_Freelance@hotmail.com>
2018-07-02 15:48:50 -0400

    Setup FlashCardPage for editing and rendering in the proper route

--- incipit/src/App.js
+++ incipit/src/App.js
@@ -4,13 +4,13 @@ import './components/bulma.css';
 import './App.css';
 import { Route } from 'react-router-dom';
 import LandingContainer from './components/LandingContainer/LandingContainer';
-
-
+import FlashCardPage from './components/FlashCardPage/FlashCardPage';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path='/' component={LandingContainer} />
+        <Route exact path='/flashcard-app' component={FlashCardPage} />
       </div>
     );
   }
---
+++ incipit/src/components/FlashCardPage/FlashCardPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavbarContainer from './Navbar/NavbarContainer';
+const FlashCardPage = () => {
+    return (
+        <div>
+            <NavbarContainer />
+        </div>
+    );
+}
+ 
+export default FlashCardPage;
