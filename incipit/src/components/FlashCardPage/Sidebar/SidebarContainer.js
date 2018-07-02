7e40307c6d661a211443059fa73d723005248af3
Simon <Simon_Freelance@hotmail.com>
2018-07-02 18:09:02 -0400

    Searchbar added

--- incipit/src/components/FlashCardPage/Sidebar/SidebarContainer.js
+++ incipit/src/components/FlashCardPage/Sidebar/SidebarContainer.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import '../../global-variables.css';
-import styled from 'styled-components';
-import '../../bulma.css';
+import React from "react";
+import "../../global-variables.css";
+import styled from "styled-components";
+import "../../bulma.css";
 
 const SidebarWrapper = styled.div`
-
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    width: 80%;
-    max-width: 400px;
-    background: var(--light-grey);
-    z-index: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  padding-top: 112px;
+  width: 80%;
+  max-width: 400px;
+  background: var(--light-grey);
+  z-index: 0;
 `;
-const SidebarContainer = (props) => {
-    return (
-        <SidebarWrapper className = {`${props.isHamburgerActive ? '' : 'is-hidden'}`}>
-        </SidebarWrapper>
-    );
-}
- 
+
+const SearchBar = styled.input.attrs({
+  type: "text",
+  className: "input is-large",
+  placeholder: "Search"
+})`
+`;
+
+const SidebarContainer = props => {
+  return (
+    <SidebarWrapper className={`${props.isHamburgerActive ? "" : "is-hidden"}`}>
+        <div class="control has-icons-left has-icons-right">
+        <SearchBar />
+          <span class="icon is-left">
+            <i class="fas fa-search" />
+          </span>
+        </div>
+    </SidebarWrapper>
+  );
+};
+
 export default SidebarContainer;
