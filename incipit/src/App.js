6be20d012c31dec0f6354af941332f50cc35eef8
Simon <Simon_Freelance@hotmail.com>
2018-07-02 15:07:30 -0400

    Router renders the landing page.

--- incipit/src/App.js
+++ incipit/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './components/global-variables.css';
 import './components/bulma.css';
+import { Route } from 'react-router-dom';
 import LandingContainer from './components/LandingContainer/LandingContainer';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingContainer />
+        <Route exact path='/' component={LandingContainer} />
       </div>
     );
   }
