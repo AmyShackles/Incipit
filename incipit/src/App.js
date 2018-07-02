aa6ffabc3a55fd7e4c0bc91a871755038d6b793b
Simon <Simon_Freelance@hotmail.com>
2018-07-02 15:00:23 -0400

    Imported LandingContainer and rendered it

--- incipit/src/App.js
+++ incipit/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './components/global-variables.css';
 import './components/bulma.css';
-
+import LandingContainer from './components/LandingContainer/LandingContainer';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LandingContainer />
       </div>
     );
   }
