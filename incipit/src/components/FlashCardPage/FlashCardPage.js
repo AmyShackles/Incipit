1d276bf1de98f1a2edbfeb8cfc794e1cb8668e78
Simon <Simon_Freelance@hotmail.com>
2018-07-02 16:15:40 -0400

    FlashCardPage constrains the max width and changes the background color

--- incipit/src/components/FlashCardPage/FlashCardPage.js
+++ incipit/src/components/FlashCardPage/FlashCardPage.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import NavbarContainer from './Navbar/NavbarContainer';
+import React from "react";
+import NavbarContainer from "./Navbar/NavbarContainer";
+import "../bulma.css";
+import styled from "styled-components";
+
+const PageWrapper = styled.div`
+  max-width: 1440px;
+  width: 95%;
+  margin: 0 auto;
+`;
 const FlashCardPage = () => {
-    return (
-        <div>
-            <NavbarContainer />
-        </div>
-    );
-}
- 
+  return (
+    <div className="hero is-dark is-fullheight">
+      <PageWrapper>
+        <NavbarContainer />
+      </PageWrapper>
+    </div>
+  );
+};
+
 export default FlashCardPage;
