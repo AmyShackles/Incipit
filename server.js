263617788139cab95f1b25c5d1f2a89fed32b075
AmyShackles <amyshackles@gmail.com>
2018-07-03 06:48:24 -0700

    .

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').load()
 const express = require('express')
 const mongoose = require('mongoose')
 
+const port = process.env.PORT || 5000
+
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware')
 const setupRoutes = require('./_config/routes');
@@ -13,13 +15,14 @@ setupRoutes(server);
 
 mongoose.Promise = global.Promise 
 
-db.connectTo('incipit')
-    .then(() => {
-        console.log('\n *** Incipit connected to database **\n')
-        server.listen(5000, () => {
-            console.log('\n*** INCIPIT is running on a port **\n')
-        })
-    })
-    .catch(err => {
-        console.log('\n *** Incipit is definitely not connected to a database ***\n')
-    })
+mongoose.connect(process.env.MLAB, {}, err => {
+    if (err) {
+        console.log(`That did not work as I had intended`)
+    } else {
+        console.log(`Incipit seems to be up and running!`)
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Incipit is running on ${port}`)
+})
