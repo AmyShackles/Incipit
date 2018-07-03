96abb06caab0b648008819e06bb196c3ccc6cbb3
Simon <Simon_Freelance@hotmail.com>
2018-07-02 21:26:29 -0400

    Started on making the addFlashCard method

--- incipit/src/App.js
+++ incipit/src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from "react";
+import "./components/global-variables.css";
+import "./components/bulma.css";
 import "./App.css";
 import { Route } from "react-router-dom";
 import AddDeckModal from "./components/Modals/AddDeckModal";
@@ -20,40 +23,71 @@ class App extends Component {
     };
   }
   makeHamburgerActive = () => {
-    this.setState({isHamburgerActive: !this.state.isHamburgerActive});
-  }
+    this.setState({ isHamburgerActive: !this.state.isHamburgerActive });
+  };
   makeAddModalActive = () => {
-    this.setState({isAddModalActive: !this.state.isAddModalActive });
+    this.setState({ isAddModalActive: !this.state.isAddModalActive });
+  };
+  makeFlashcardModalActive = () => {
+    this.setState({ isFlashcardModalActive: !this.state.isFlashcardModalActive });
   }
 
   addDeck = () => {
     const deckPanels = this.state.deckPanels;
-    const deckPanel = { name: this.state.deckName, id: Date.now(), category: 'public'}
+    const deckPanel = {
+      name: this.state.deckName,
+      id: Date.now(),
+      category: "public"
+    };
     deckPanels.push(deckPanel);
 
-    this.setState({ deckPanels, deckName: '' });
-  }
-  detectChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-  }
+    this.setState({ deckPanels, deckName: "" });
+  };
+  addFlashCard = () => {
+    const flashcards = this.state.flashcards;
+    const flashcard = {
+      front: this.state.frontInfo,
+      back: this.state.backInfo,
+      likes: 0,
+      dislikes: 0,
+      id: Date.now()
+    };
+    flashcards.push(flashcard);
+
+    this.setState({ flashcards });
+  };
+  detectChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={LandingContainer} />
+        <Route exact path="/" component={LandingContainer} />
 
+        <Route
+          exact
+          path="/flashcard-app"
+          render={props => (
+            <FlashCardPage
+              {...props}
+              isHamburgerActive={this.state.isHamburgerActive}
+              hamburgerHandler={this.makeHamburgerActive}
+              addModalHandler={this.makeAddModalActive}
+              deckPanels={this.state.deckPanels}
+        flashCardModalHandler = {this.makeFlashcardModalActive}
 
-        <Route exact path='/flashcard-app' render={props => <FlashCardPage {...props} 
-        isHamburgerActive = {this.state.isHamburgerActive}
-        hamburgerHandler = {this.makeHamburgerActive}
-        addModalHandler = {this.makeAddModalActive}
-        deckPanels = {this.state.deckPanels}
-        />}
+            />
+          )}
         />
 
-
-
-        <AddDeckModal addModalActive = {this.state.isAddModalActive}
-        addModalHandler={this.makeAddModalActive}
+        <AddDeckModal
+          addModalActive={this.state.isAddModalActive}
+          addModalHandler={this.makeAddModalActive}
+          addDeckHandler={this.addDeck}
+          changeHandler={this.detectChange}
+          addDeckHandler={this.addDeck}
+          deckName={this.state.deckName}
+        />
         <AddFlashcardModal 
         flashCardModalActive = {this.state.isFlashcardModalActive}
         changeHandler = {this.detectChange}
