9a4679b69dbbed817b74e8b72232d9027b7f2296
Simon <Simon_Freelance@hotmail.com>
2018-07-02 22:16:53 -0400

    Did the styling for the FlashcardAdder component

--- incipit/src/components/Flashcard-container/FlashcardAdder.js
+++ incipit/src/components/Flashcard-container/FlashcardAdder.js
@@ -3,9 +3,29 @@ import '../bulma.css';
 import '../global-variables.css';
 import styled from 'styled-components';
 
+const FlashcardContainer = styled.div`
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 350px;
+background: var(--grey);
+cursor: pointer;
+color: var(--brand-white);
+
+`;
+
+const Plus = styled.h2`
+    font-size: 9.6rem;
+
+`;
 const FlashCardAdder = (props) => {
     return (
-        <div></div>
+        <FlashcardContainer className='box'>
+            <Plus>+</Plus>
+            <p>Add a new card</p>
+        </FlashcardContainer>
     )
 }
  
