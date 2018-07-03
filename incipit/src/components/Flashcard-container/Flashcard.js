fa72b227f14ee0cec3d5a60b96bcf7ffce32f027
Simon <Simon_Freelance@hotmail.com>
2018-07-02 23:01:15 -0400

    Flashcard flips when clicked on.

--- incipit/src/components/Flashcard-container/Flashcard.js
+++ incipit/src/components/Flashcard-container/Flashcard.js
@@ -1,20 +1,65 @@
-import React from 'react';
-import '../bulma.css';
-import '../global-variables.css';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import "../bulma.css";
+import "../global-variables.css";
+import styled from "styled-components";
 
-const FlashcardRectangle = styled.div.attrs({
-    className: 'box'
-})`
-    height: 200px;
-`;
-
-const Flashcard = (props) => {
-    return (
-        <div className = 'box'>
-            
-        </div>
-    );
-}
+const FlashcardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 350px;
+  cursor: pointer;
+  color: var(--brand-white);
+  perspective: 600px;
  
+`;
+const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  &.is-flipped {
+    transform: rotateX(180deg);
+  }
+`;
+const CardFront = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: var(--grey);
+  backface-visibility: hidden;
+`;
+const CardBack = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: var(--grey);
+  backface-visibility: hidden;
+  transform: rotateX(180deg);
+`;
+class Flashcard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFlipped: false
+    };
+  }
+  render() {
+    return (
+      <FlashcardContainer onClick = { () => {
+          
+            this.setState({ isFlipped: !this.state.isFlipped })
+          }
+      }>
+        <Card className = {`${this.state.isFlipped ? 'is-flipped' : ''}`}>
+          <CardFront>front</CardFront>
+          <CardBack >back</CardBack>
+        </Card>
+      </FlashcardContainer>
+    );
+  }
+}
+
 export default Flashcard;
